c9afe60224e3bff5be70a5d013229facda208d80
duyguca <duygukoksalca@gmail.com>
2024-02-05 11:09:06 -0800

    feat:button added

---
+++ .storybook/main.ts
@@ -0,0 +1,27 @@
+import type { StorybookConfig } from "@storybook/nextjs";
+import path from "path";
+
+const config: StorybookConfig = {
+  webpackFinal: async (config, { configType }) => {
+    if (config?.resolve?.modules) {
+      config?.resolve?.modules.push(path.resolve(__dirname, "../"));
+    }
+
+    return config;
+  },
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-onboarding",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/nextjs",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+};
+export default config;
---
+++ .storybook/preview.ts
@@ -0,0 +1,27 @@
+import type { Preview } from "@storybook/react";
+import "../src/app/globals.css";
+import { withThemeByClassName } from "@storybook/addon-themes";
+
+export const decorators = [
+  withThemeByClassName({
+    themes: {
+      light: "light",
+      dark: "dark",
+    },
+    defaultTheme: "light",
+  }),
+];
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
